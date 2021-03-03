c676d79660cd8817c151e81aa8d369bc225302ae
ogwtkhr <color_me_pop@live.jp>
2021-03-03 09:27:23 +0900

    PIXIセットアップ

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
+import * as PIXI from 'pixi.js';
 
+const main = () => {
+  const app = new PIXI.Application({
+    width: 400,
+    height: 200,
+  });
+  document.body.append(app.view);
+  const text = new PIXI.Text('Hello World!');
+  text.style.fill = '#ffffff';
+  app.stage.addChild(text);
+};
+
+window.addEventListener('load', main);
